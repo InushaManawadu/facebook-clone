b8462da976c2fdae21116a42a6745e19ecbbd613
InushaManawadu <66345099+InushaManawadu@users.noreply.github.com>
2020-09-04 00:23:39 +0530

    Header Completed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-      <h1>Lets Build a FB clone</h1>
+      <h1></h1>
 
       <Header />
 
--- src/Header.js
+++ src/Header.js
@@ -6,21 +6,26 @@ import FlagIcon from '@material-ui/icons/Flag';
 import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
 import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
 import SupervisedUserCircleOutlinedIcon from '@material-ui/icons/SupervisedUserCircleOutlined';
+import { Avatar } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Header() {
     return (
         <div className='header'>
-
             <div className="header__left">
                 <img src="https://img.icons8.com/cute-clipart/64/000000/facebook-new.png"/>
                 <div className="header__input">
                     <SearchIcon />
-                    <input type="text" />
+                    <input placeholder="Search Facebook" type="text" />
                 </div>
             </div>
             
-            <div className="header__middle">
-                <div className="header__option">
+            <div className="header__center">
+                <div className="header__option header__option--active">
                     <HomeIcon fontSize="large" />
                 </div>
                 <div className="header__option">
@@ -38,7 +43,22 @@ function Header() {
             </div>
             
             <div className="header__right">
-
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Inusha Manawadu</h4>
+                </div>
+            <IconButton>
+                <AddIcon />
+            </IconButton>
+            <IconButton>
+                <ForumIcon />
+            </IconButton>
+            <IconButton>
+                <NotificationsActiveIcon />
+            </IconButton>
+            <IconButton>
+                <ExpandMoreIcon />
+            </IconButton>
             </div>
         </div>
     )
